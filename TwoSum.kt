d3cfe50ca9dfcd1223ce6465bdeaebceb51eecf5
Samim Hakimi <30713537+sammyHa@users.noreply.github.com>
2019-07-22 11:31:54 -0700

    Update TwoSum.kt

--- TwoSum.kt
+++ TwoSum.kt
@@ -10,46 +10,76 @@ Given a list of numbers and a number k, return whether any two numbers from the 
 For example, given [10, 15, 3, 7] and k of 17, return true or print the indices of the two values in the list since 10 + 7 is 17.
  */
 
-
-fun twoSum(list: List<Int>, k:Int):Boolean{
-    for (i in list){
-        for (j in list){
-            if (i != j && i + j == k){
-                return true
-            }
-        }
-    }
-    return false
-}
-
-
-fun twoSum1(list:List<Int>, k:Int):Boolean{
-    val seen = mutableListOf<Int>()
-    for (i in list){
-        if (k - i in seen){
+/**
+ *  Time complexity: O(n) we iterate through the list only once.
+ *  Space complexity: O(1) because we are not using any auxiliary data structure. 
+ */
+fun twoSum(nums: List<Int>, target: Int):Boolean{
+    var i = 0 // i starts at the index zero
+    var j = nums.size - 1 // j start at the last index
+    while (i<= j){
+        if (nums[i] + nums[j] == target){
+            println("${nums[i]}, ${nums[j]} at index $i, $j")
             return true
+        }else if (nums[i] + nums[j] < target){ // if the value at the index i + j is less than the target goto the next index.
+            i+= 1
+        }else{ // if the value at the index i + j is greater than the target decrease (j--)
+            j -= 1
         }
-        seen.add(i)
     }
     return false
 }
 
+/**
+* Timce complexity: O(n^2)
+*/
+// fun twoSum(list: List<Int>, k:Int):Boolean{
+//     for (i in list){
+//         for (j in list){
+//             if (i != j && i + j == k){
+//                 return true
+//             }
+//         }
+//     }
+//     return false
+// }
 
-private fun twoSum(nums: IntArray, target:Int):IntArray {
+/**
+* Time complexity : O(n^2)
+* Space complexity: O(n)
+*/
+// fun twoSum1(list:List<Int>, k:Int):Boolean{
+//     val seen = mutableListOf<Int>()
+//     for (i in list){
+//         if (k - i in seen){
+//             return true
+//         }
+//         seen.add(i)
+//     }
+//     return false
+// }
 
-    val hashMap = HashMap<Int, Int>()
-    for (i in 0 until nums.size){
-        if (hashMap.containsKey(target - nums[i])){
-            val intArray = intArrayOf(hashMap.get(target- nums[i])!!, i)
-                println(intArray.joinToString ())
-            return intArray // return the indexes
-        }
-        hashMap.put(nums[i], i)
-    }
-    val intArray = intArrayOf(-1,-1)
-    print(intArray.joinToString ())
-    return intArray
-}
+
+
+/**
+* Time complexity : O(n)
+* Space complexity: O(n) because we are using hashmap and the time complexity for the hashmap is O(n)
+*/
+// private fun twoSum(nums: IntArray, target:Int):IntArray {
+
+//     val hashMap = HashMap<Int, Int>()
+//     for (i in 0 until nums.size){
+//         if (hashMap.containsKey(target - nums[i])){
+//             val intArray = intArrayOf(hashMap.get(target- nums[i])!!, i)
+//                 println(intArray.joinToString ())
+//             return intArray // return the indexes
+//         }
+//         hashMap.put(nums[i], i)
+//     }
+//     val intArray = intArrayOf(-1,-1)
+//     print(intArray.joinToString ())
+//     return intArray
+// }
 
 
 fun main() {
